c35b40c548b5650510a0e63c4368177a65a98333
Guillaume Briday <guillaumebriday@gmail.com>
2018-01-02 23:20:58 +0100

    Adding font-awesome fonts

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,16 @@ module.exports = {
         options: {
           emitWarning: true
         }
+      },
+      {
+        test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
       }
     ]
   },

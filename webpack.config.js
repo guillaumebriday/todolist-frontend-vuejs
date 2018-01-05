5b9edc0c91e22b3705a4a670cb9bf87a9a91b7f4
Guillaume Briday <guillaumebriday@gmail.com>
2018-01-05 10:40:26 +0100

    Updating axios config

--- webpack.config.js
+++ webpack.config.js
@@ -97,7 +97,8 @@ if (inProduction) {
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
+        NODE_ENV: '"production"',
+        API_URL: '"http://localhost/api/v1/"'
       }
     }),
     new webpack.optimize.UglifyJsPlugin({

5e19715d8baa003ad1e729f28cd6221448b87b9e
Guillaume Briday <guillaumebriday@gmail.com>
2018-01-05 01:38:53 +0100

    Adding Navbar component

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import axios from 'axios'
+import Navbar from './components/Navbar'
 
 window.Vue = Vue
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
+Vue.component('Navbar', Navbar)
 
 window.axios = axios
 
---
+++ src/js/components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="bg-indigo" v-if="isLogged">
+    <div class="container mx-auto px-8 py-4">
+      <div class="flex justify-between">
+        <div>
+          <router-link class="text-white no-underline font-bold text-3xl hover:underline" to="/" exact>
+            Todolist
+          </router-link>
+        </div>
+        <div>
+          <button @click="logout" class="text-grey-light mr-3 border border-white py-2 px-4 rounded hover:bg-white hover:text-indigo">
+            Logout
+          </button>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Navbar',
+
+  computed: mapGetters([
+    'isLogged'
+  ]),
+
+  methods: {
+    logout () {
+      this.$store.dispatch('logout')
+    }
+  }
+}
+</script>

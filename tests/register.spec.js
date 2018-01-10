b64e4a585343cab372a5119888bd9b3646aa5f80
Guillaume Briday <guillaumebriday@gmail.com>
2018-01-10 12:20:38 +0100

    Adding a register component

---
+++ tests/register.spec.js
@@ -0,0 +1,35 @@
+import { mount } from 'vue-test-utils'
+import Register from '../src/js/components/Register.vue'
+import expect from 'expect'
+
+/* eslint-disable no-undef */
+describe('Register', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = mount(Register)
+  })
+
+  it('does not contain error alert', () => {
+    expect(wrapper.html()).not.toContain('Whoops, looks like something went wrong. Please try again.')
+  })
+
+  it('enables the register button', () => {
+    wrapper.setData({
+      name: 'frodo',
+      email: 'frodo@baggins.sh',
+      password: 'my_precious',
+      password_confirmation: 'my_precious'
+    })
+
+    let button = wrapper.find('button')
+
+    expect(button.attributes().disabled).not.toBe('disabled')
+  })
+
+  it('disables the register button', () => {
+    let button = wrapper.find('button')
+
+    expect(button.attributes().disabled).toBe('disabled')
+  })
+})

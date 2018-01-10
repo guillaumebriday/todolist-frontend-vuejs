1c9932f46d805046ee996dc755074555f62867dc
Guillaume Briday <guillaumebriday@gmail.com>
2018-01-10 09:45:13 +0100

    Removing useless assignations

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -5,13 +5,10 @@ import Navbar from './components/Navbar'
 
 const API_URL = process.env.API_URL || 'http://localhost/api/v1/'
 
-window.Vue = Vue
 Vue.config.productionTip = false
 
 Vue.use(VueRouter)
 Vue.component('Navbar', Navbar)
-
-window.axios = axios
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.token

0e17de7170c9cfc785a508ec7fc6d6fdff3a9d02
Guillaume Briday <guillaumebriday@gmail.com>
2018-05-01 19:40:49 +0200

    Adding links to home

--- src/js/components/auth/Login.vue
+++ src/js/components/auth/Login.vue
@@ -1,7 +1,11 @@
 <template>
 <div class="h-screen flex justify-center items-center">
   <div class="w-full max-w-xs">
-    <h1 class="text-center mb-6">Todolist</h1>
+    <h1 class="text-center mb-6">
+      <router-link class="text-black no-underline hover:underline" :to="{ name: 'Home'}" exact>
+        Todolist
+      </router-link>
+    </h1>
 
     <form @submit.prevent="login" @keydown="form.errors.clear($event.target.name)" class="form-card">
       <div class="mb-4">

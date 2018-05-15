6cba6b824d667581155482c008c4163a9998e852
Guillaume Briday <guillaumebriday@gmail.com>
2018-05-15 23:22:46 +0200

    Adding loading button component

---
+++ src/js/components/LoadingButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <button :type="type" :disabled="disabled">
+    <i v-if="isLoading" class="fa fa-spinner fa-spin mr-1"></i>
+    <i v-else-if="icon" :class="icon" class="fa mr-1"></i>
+
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    isLoading: {
+      type: Boolean,
+      default: false
+    },
+
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+
+    type: {
+      type: String,
+      default: 'submit'
+    },
+
+    icon: {
+      type: String,
+      default: null
+    }
+  }
+}
+</script>

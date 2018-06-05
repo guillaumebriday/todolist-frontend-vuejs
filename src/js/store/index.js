6c5770d3d13395ccf7ffea043f1f6e2285e0cd24
Guillaume Briday <guillaumebriday@gmail.com>
2018-06-05 15:33:13 +0200

    Adding a tasks module in store

--- src/js/store/index.js
+++ src/js/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import auth from '@/store/modules/auth'
+import tasks from '@/store/modules/tasks'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    tasks
   }
 })

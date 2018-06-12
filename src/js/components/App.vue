c9c191dd80ed4a16189b71f25cc7f9a424e1f45b
Guillaume Briday <guillaumebriday@gmail.com>
2018-06-12 17:22:38 +0200

    Adding an interceptor to refresh a token if needed

--- src/js/components/App.vue
+++ src/js/components/App.vue
@@ -6,11 +6,29 @@
 
 <script>
 import axios from 'axios'
+import moment from 'moment'
 
 export default {
   created () {
+    axios.interceptors.request.use(config => {
+      let expiresAt = moment(window.localStorage.getItem('expiresAt'))
+
+      if (expiresAt.isValid() && moment().isBefore(expiresAt)) {
+        let diff = moment.duration(expiresAt.diff(moment()))
+
+        if (diff.asHours() < 12 && config.url !== 'auth/refresh') {
+          axios.post('auth/refresh')
+            .then(({ data }) => {
+              this.$store.commit('LOGIN', data)
+            })
+        }
+      }
+
+      return config
+    })
+
     axios.interceptors.response.use(response => response, error => {
-      if (error.response.status === 401) {
+      if (error.response && error.response.status === 401) {
         this.$store.dispatch('logout')
       }
 

3213506e120a17d64eda2350541f3ae3db189146
Guillaume Briday <guillaumebriday@gmail.com>
2018-09-11 22:31:07 +0200

    Adding the Vue Plugin with the router

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap'
 
 import Vue from 'vue'
+import VueAnalytics from 'vue-analytics'
 import App from '@components/App'
 import router from '@router'
 import store from '@store'
@@ -8,6 +9,14 @@ import moment from 'moment'
 require.context('../images', true)
 
 moment.tz.setDefault(moment.tz.guess())
+
+Vue.use(VueAnalytics, {
+  id: process.env.TRACKING_ID,
+  router,
+  debug: {
+    sendHitTask: process.env.NODE_ENV === 'production'
+  }
+})
 
 /* eslint-disable no-new */
 new Vue({
